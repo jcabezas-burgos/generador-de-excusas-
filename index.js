0ab5ef20af4bcfb331c6f6e0ef8bee96eed9e51f
Jcabezas.burgos <jcabezas.burgos@gmail.com>
2022-08-27 01:05:35 +0000

    generador de excusas

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+document.querySelector = (Math.floor(Math.random()*who.length)); 	
+document.querySelector  = (Math.floor(Math.random()*what.length));	
+document.querySelector = (Math.floor(Math.random()*when.length)); 
+
+var who1 = document.querySelector = (who [Math.floor(Math.random()*who.length)]); 
+var what1 = document.querySelector  = (what [Math.floor(Math.random()*what.length)]);
+var when1 = document.querySelector = (when [Math.floor(Math.random()*when.length)]); 
+   
+
+
+console.log(who1 + " " + what1 + " " + when1)
+
+
+
