b2fc8991b43ccc582df4663a1ca351e8998d3ba8
Jcabezas.burgos <jcabezas.burgos@gmail.com>
2022-08-29 23:22:33 +0000

    actualizado

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ let who = ['the dog','my granma','his turtle','my bird'];
 let what = ['eat','pissed','crushed','broked'];
 let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
-document.querySelector = (Math.floor(Math.random()*who.length)); 	
-document.querySelector  = (Math.floor(Math.random()*what.length));	
-document.querySelector = (Math.floor(Math.random()*when.length)); 
+
 
 var who1 = document.querySelector = (who [Math.floor(Math.random()*who.length)]); 
 var what1 = document.querySelector  = (what [Math.floor(Math.random()*what.length)]);
